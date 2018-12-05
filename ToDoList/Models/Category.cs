0c5b8f16d104330a9d7e2525b83c115e6b348cbe
Cristian Lucero <cristianjlucero32@gmail.com>
2018-12-05 07:49:25 -0800

    Latest working ToDoList 12/4/18

---
+++ ToDoList/Models/Category.cs
@@ -0,0 +1,85 @@
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+
+namespace ToDoList.Models
+{
+  public class Category
+  {
+    private static List<Category> _instances = new List<Category> {};
+    private string _name;
+    private int _id;
+    private List<Item> _items;
+
+    public Category(string categoryName)
+    {
+      _name = categoryName;
+      _instances.Add(this);
+      _id = _instances.Count;
+      _items = new List<Item>{};
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public void AddItem(Item item)
+    {
+      _items.Add(item);
+    }
+
+    public static void ClearAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM categories;";
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public static List<Category> GetAll()
+    {
+      List<Category> allCategories = new List<Category> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM categories;";
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int CategoryId = rdr.GetInt32(0);
+        string CategoryName = rdr.GetString(1);
+        Category newCategory = new Category(CategoryName);
+        allCategories.Add(newCategory);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allCategories;
+    }
+
+    public static Category Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+
+    public List<Item> GetItems()
+    {
+      return _items;
+    }
+
+  }
+}
